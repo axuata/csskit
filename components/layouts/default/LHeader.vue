37a333aca9c0a1e331104dbc30a2ddd61518026d
axuata <axuata@gmail.com>
2025-05-27 15:19:13 +0900

    🪲 Fixed the header home link bug (doesn't navigate to / with the current locale)

--- components/layouts/default/LHeader.vue
+++ components/layouts/default/LHeader.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
-const {t, locales, locale, setLocale} = useI18n();
-const homePath = `/${locale.value}/`;
+const {t, locales, setLocale} = useI18n();
+const currentLocale = ref<string>('ja');
+const homePath = computed(() => {
+  return `/${currentLocale.value}/`;
+});
+
+function onLocaleClick(localeCode: any) {
+  setLocale(localeCode);
+  currentLocale.value = localeCode;
+  console.log(currentLocale.value)
+}
 </script>
 
 <template>
-  <header class="bg-white b-b-(solid 1px gray-2)">
+  <header class="bg-white b-b-(solid 1px gray-2) px-10px">
     <div class="max-w-1200px mx-auto flex items-center justify-between h-55px">
       <div class="flex gap-5px items-center">
         <Icon name="twemoji:artist-palette" class="size-22px" />
@@ -16,7 +25,7 @@ const homePath = `/${locale.value}/`;
       <div class="flex items-center gap-12px text-13px text-gray-7 font-500">
         <NuxtLink :to="homePath" class="hover:tracking-wider transition-all duration-100">{{ t('layouts.header.home') }}</NuxtLink>
         <select class="bg-white rounded-8px px-5px py-3px transition-colors duration-100 cursor-pointer hover:bg-gray-1 w-fit">
-          <option v-for="locale in locales" @click="setLocale(locale.code)">{{ locale.name }}</option>
+          <option v-for="locale in locales" @click="onLocaleClick(locale.code)">{{ locale.name }}</option>
         </select>
       </div>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@ const features = [
 <template>
   <div>
     <!-- Hero Section -->
-    <section class="max-w-1200px mx-auto py-70px">
+    <section class="max-w-1200px mx-auto py-70px max-md:px-10px">
       <div class="text-44px line-height-tight font-600 mb-16px">
         <p>CSS Kit</p>
         <p v-html="t('pages.home.html_hero')"></p>
@@ -40,7 +40,7 @@ const features = [
     </section>
 
     <!-- Tools Section -->
-    <section class="bg-#f3f8fc min-h-300px py-24px">
+    <section class="bg-#f3f8fc min-h-300px py-24px max-md:px-10px">
       <div class="max-w-1200px mx-auto">
         <h1 class="text-24px font-600 text-gray-9">{{ t('pages.home.all_tools') }}</h1>
         <div class="flex flex-wrap gap-22px pt-4px">
